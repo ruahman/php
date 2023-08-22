d0af80b8867a74ccf3136d19e2e946c31d5ec8a2
diegovelascommerce <diego@velacommerce.com>
2023-08-21 20:16:49 -0400

    added blog to laravel

---
+++ laravel/blog/resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    </article>
+    <!-- <script src="./app.js"></script> -->
+</body>
+
+</html>
